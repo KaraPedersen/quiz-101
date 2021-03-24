9955de4e6c6960c6644f3bd78f38e765d35fdf94
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-03-23 17:02:29 -0700

    added color to background

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 ## Making a plan
-1) Make a drawing of your app. Simple "wireframes"
-2) Once you have a drawing, name the HTML elements you'll need to realize your vision
-3) For each HTML element ask: Why do I need this?
-    - This is your pseudocode
-4) Once we know _why_ we need each element, think about how to implement the "Why" as a "How"
-5) Find all the 'events' (user clicks, form submit, etc) in your app. Ask one by one, "What happens when" for each of these events.
-6) Think about how to validate each of your steps
+We need a button with an id 
+    Why? to show the results of the quiz to user
+    How? someElement.textContent = 'results...'
+
+What does it mean to launch a quiz? 
--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 // import functions and grab DOM elements
+import { isYes } from './utils.js';
+
+const button = document.getElementById('launch-button');
+const resultsDiv = document.getElementById('results')
 
 // initialize state
 
 // set event listeners to update state and DOM
+button.addEventListener('click', () => {
+    alert('Welcome to the ice cream quiz!');
+
+    const wantsToTakeQuiz = confirm ('Do you want to take the quiz?');
+
+    if (!wantsToTakeQuiz) {
+        alert('Alrighty then. Enjoy your Ice Cream!!');
+        return;
+    }
+
+    const name = prompt('What is your name');
+
+    let score = 0;
+
+    const firstAnswer = prompt('Is ice cream a frozen desert?');
+    const secondAnswer = prompt('Are fruits frequently used in the making of ice cream?');
+    const thirdAnswer = prompt('Is ice cream only made with sugar?');
+
+    if (isYes(firstAnswer)) score++;
+    if (isYes(secondAnswer)) score++;
+    if (!isYes(thirdAnswer)) score++;
+
+    const resultsString = `Fantastic! ${name} you got ${score} right out of three!!`;
+
+
+    resultsDiv.textContent = resultsString;
+
+});
